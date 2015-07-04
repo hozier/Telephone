0b966f589835a755f402f032582fdea94699422b
Philron William Hozier <phozier@Philrons-MacBook-Air.local>
2015-07-04 16:03:39 -0400

    Initial Commit

---
+++ Telephone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Telephone
+//
+//  Created by Philron William Hozier on 7/4/15.
+//  Copyright (c) 2015 Philron William Hozier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Telephone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Telephone
+//
+//  Created by Philron William Hozier on 7/4/15.
+//  Copyright (c) 2015 Philron William Hozier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
